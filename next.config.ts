7c5587e8da9a21701e9d98ffca6ecf3df3c42130
Abdul-Saidat <yewandeabdul822@gmail.com>
2025-09-25 05:02:04 +0100

    fixed configuration error in next.config file

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root:  "C:\\Users\\GBEMISOLA\\OneDrive\\Documents\\my-todo-app"
-  }
 };
 
 export default nextConfig;
